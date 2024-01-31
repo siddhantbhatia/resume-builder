07a4bdaefa6bfe7557979d3288408c08a37a73f8
Siddhant Bhatia <siddhant8b@gmail.com>
2024-01-31 14:37:26 +0800

    replace env with header host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { TemplateListing } from "./components/TemplateListing";
 import { TemplateListingResponse } from "./types/templateListingResponse";
 
+import { headers } from "next/headers";
+
 import styles from "./page.module.css";
 
 async function getData() {
-  const res = await fetch(`http://${process.env.domain}/api/templates`);
+  const headersList = headers();
+
+  const domain = headersList.get("host");
+  const res = await fetch(`http://${domain}/api/templates`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
